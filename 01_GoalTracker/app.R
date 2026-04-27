b192734a3e94aee2c434207e739c3ab522ad0f94
Mohammad Sayeef Alam <alam.sayeef@gmail.com>
2026-04-27 12:50:15 +0100

    bslib theme + dashboard and input panel

--- 01_GoalTracker/app.R
+++ 01_GoalTracker/app.R
@@ -2,10 +2,13 @@ library(shiny)
 library(bslib)
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+ui <- page_navbar(
+  theme = bs_theme(version = 5),
+  title = "Goals tracker",
+  nav_panel("Dashboard",
+            "Cards and value boxes go here"),
+  nav_panel("Inputs",
+            "this is we")
 )
 
 server <- function(input, session, output){
